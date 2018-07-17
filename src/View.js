1fd2b0187945608c195c23824d89ab7928e72864
Evan Brass <brassevan@gmail.com>
2018-07-17 16:31:15 -0700

    Start laying out how I want the interface of the view to work

--- src/View.js
+++ src/View.js
@@ -2,7 +2,32 @@
 
 import { parameterList } from "./common.js";
 
-export default function View(model, func) {
 
+// View keeps track of the users that have been added to a perticular model and activates/deactivates/unuses them with the lifecycle of the view.
+export default function View(model, func) {
+	
 
 };
+
+// How I'd like to be able to use this:
+
+let documentFragment = View(model, function (one, two, unsafe, link) {
+	// Static variables
+	let ratings = [
+		'Inteligent',
+		'Bland',
+		'Uninteligent',
+		'Uninteligable'
+	];
+
+	return
+html`<section class="post">
+	<header>
+		<select>
+			${ratings.map(rating => html`<option>${rating}</option>`)}
+		</select>
+		<h1>${one(author)}</h1>
+	</header>
+	${unsafe(content)}
+</section>`;
+})
